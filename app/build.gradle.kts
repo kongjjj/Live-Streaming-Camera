154752b1edc6401255cc4360b8c3558780d5fcab
kongjjj <kongjjj@users.noreply.github.com>
2026-03-30 19:43:56 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,21 +2,18 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "io.github.thibaultbee.streampack.app"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.kongjjj.livestreamingcamera"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "io.github.thibaultbee.streampack.app"
+        applicationId = "com.kongjjj.livestreamingcamera"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 38
+        versionName = "2.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -30,35 +27,44 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_18
         targetCompatibility = JavaVersion.VERSION_18
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_18)
-        }
-    }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
-dependencies {
-    implementation(libs.streampack.core)
-    // For the `PreviewView`
-    implementation(libs.streampack.ui)
-    // TODO: Only needed for RTMP live streaming: remove if you don't need it
-    implementation(libs.streampack.rtmp)
-    // TODO: Only needed for SRT live streaming: remove if you don't need it
-    implementation(libs.streampack.srt)
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_18)
+    }
+}
 
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    
+    implementation(libs.streampack.core)
+    implementation(libs.streampack.ui)
+    implementation(libs.streampack.rtmp)
+    implementation(libs.streampack.srt)
+    implementation(libs.okhttp)
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.lifecycle.runtime.ktx)
-
+    implementation(libs.activity)
+    implementation(libs.preference)
+    implementation(libs.xlog)
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
