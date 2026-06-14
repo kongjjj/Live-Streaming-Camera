f4c69dbf6074f57fa4233550843871d604042eab
kongjjj <kongjjj@users.noreply.github.com>
2026-06-14 14:53:09 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kongjjj.livestreamingcamera"
         minSdk = 24
         targetSdk = 36
-        versionCode = 77
-        versionName = "2.5.8"
+        versionCode = 78
+        versionName = "2.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
