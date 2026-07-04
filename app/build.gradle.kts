67335518b8690ba3125a2e32db3f84588190dc04
kongjjj <kongjjj@users.noreply.github.com>
2026-07-04 14:00:29 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kongjjj.livestreamingcamera"
         minSdk = 24
         targetSdk = 36
-        versionCode = 83
-        versionName = "2.6.4"
+        versionCode = 85
+        versionName = "2.6.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
